d6f701f4bc48636e2ca9a3fb084199fbe461995d
vastavikadi <aditya82004shukla@gmail.com>
2025-08-11 18:42:50 +0000

    arrays in go

---
+++ 08array/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+//fmt.Println("This is a class lecture about arrays but arrays are not much used in golang like other prorgramming lanuages. Now lets study more about this topic.")
+var fruitList [4]string//here we are just defining and not initialising
+fruitList[0] = "Apple"
+fruitList[1] = "Tomato"
+fruitList[3] = "Apple"
+fmt.Println("Fruit list is: ", fruitList)//this is output [Apple Tomato  Apple]notice there is an extra space between tomato and apple
+fmt.Println("Length of the array is: ", len(fruitList))//does not matter if there is only 3 values inside the array once the length is declared tht is waht it is gonna return not gonna count the actualy no of values inside the array
+
+var vegList = [3]string{"potato", "tomato", "apple"}//here even we make it 5 and give 3 values and then print the length then the length is gonna be 5
+fmt.Println("The vegList is: ", vegList)
+fmt.Println("The length of the vegList is: ", len(vegList))
+}
