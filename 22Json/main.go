51e6a6c98b8d7d530e25b3fe772e4a1f3d5471f3
vastavikadi <aditya82004shukla@gmail.com>
2025-08-15 00:16:57 +0530

    json handling

--- 22Json/main.go
+++ 22Json/main.go
@@ -90,4 +90,94 @@ func main() {
 	//                 "website": "Lco.in"
 	//         }
 	// ]
+
+	//decoded json
+	fmt.Println("DECODED JSON: -")
+	DecodeJson() //this is the output: [{Reactjs Bootcamp 299 Lco.in  [webdev js]} {GOlang Course 0 Lco.in  [webdev go]} {Web3 59 Lco.in  []}]
+	fmt.Println("STRUCT JSON: -")
+	makeStructFromJSON()
 }
+
+func DecodeJson() {
+	jsonData := []byte(`
+	[
+        {
+                "coursename": "Reactjs Bootcamp",
+                "Price": 299,
+                "website": "Lco.in",
+                "tags": [
+                        "webdev",
+                        "js"
+                ]
+        },
+        {
+                "coursename": "GOlang Course",
+                "Price": 0,
+                "website": "Lco.in",
+                "tags": [
+                        "webdev",
+                        "go"
+                ]
+        },
+        {
+                "coursename": "Web3",
+                "Price": 59,
+                "website": "Lco.in"
+        }
+]`)
+
+	var lcoCourses []course
+
+	checkValid := json.Valid(jsonData) //to check the validity(means the structure of the json is correct)
+	fmt.Println(checkValid)            // returns true but as soon as ek comma bhi aaya then its wrong
+
+	if checkValid {
+		fmt.Println("JSON WAS VALID")
+		json.Unmarshal(jsonData, &lcoCourses) //Unmarshal parses the JSON-encoded data and passing the &lcoCourses because it stores in the original
+
+		fmt.Printf("%#v", lcoCourses) //[]main.course{main.course{Name:"Reactjs Bootcamp", Price:299, Platform:"Lco.in", Password:"", Tags:[]string{"webdev", "js"}}, main.course{Name:"GOlang Course", Price:0, Platform:"Lco.in", Password:"", Tags:[]string{"webdev", "go"}}, main.course{Name:"Web3", Price:59, Platform:"Lco.in", Password:"", Tags:[]string(nil)}}
+		// fmt.Println(lcoCourses)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+}
+
+func makeStructFromJSON() {
+	// some cases where you just want to add data to key value
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"website": "LearnCodeOnline.in",
+		"tags": ["web-dev","js"]
+	}
+	`)
+
+	// map[string]interface{} only works if the root JSON value is an object starts with {} as jsonDataFromWeb not like jsonData
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Println("STRUCT FROM JSON: -")
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v  and value is %v and Type is: %T\n", k, v, v)
+	}
+	//used when
+	// 	Don’t know the JSON structure ahead of time (or don’t want to create a Go struct for it).
+	// 	Still want to access JSON fields dynamically in Go.
+}
+
+
+//NOTE for : Unmarshal
+// Here’s what’s happening under the hood:
+// map[string]interface{} means:
+// Keys are always strings (just like JSON object keys).
+// Values are stored as interface{} so they can hold any JSON type — strings, numbers, booleans, arrays, nested maps, etc.
+
+// When json.Unmarshal reads JSON into this type:
+// JSON objects ({ ... }) → become map[string]interface{}
+// JSON arrays ([ ... ]) → become []interface{}
+// JSON numbers → become float64 by default
+// JSON strings → become string
+// JSON booleans → become bool
+// JSON null → becomes nil
